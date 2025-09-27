1263c483dd5f4e36302286475f61e41e3390dbdc
MichalPorazko <michal.porazko@protonmail.com>
2025-09-27 19:27:55 +0200

    Some corrections in n the main.c file

--- STM32_TFT_ST7735S_menu/Core/Src/main.c
+++ STM32_TFT_ST7735S_menu/Core/Src/main.c
@@ -25,10 +25,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
-#include "lcd.h"
-#include "hagl.h"
-#include "font6x9.h"
-#include "rgb565.h"
+#include "menu.h"
 
 /* USER CODE END Includes */
 
@@ -61,14 +58,6 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
-{
-	if (hspi == &hspi2)
-	{
-		lcd_transfer_done();
-	}
-}
 
 /* USER CODE END 0 */
 
@@ -105,24 +94,7 @@ int main(void)
   MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
-  hagl_clear_screen();
   lcd_init();
-
-  color_t gold = rgb565(255, 215, 0);
-  color_t red = rgb565(255, 0, 0);
-
-  hagl_draw_rectangle(0, 0, LCD_WIDTH - 1, LCD_HEIGHT - 1, gold);
-
-  int rect_width = 80;
-  int rect_height = 30;
-  int x = (LCD_WIDTH - rect_width) / 2;
-  int y_top = 20;
-
-  hagl_fill_rectangle(x, y_top, x + rect_width - 1, y_top + rect_height - 1, red);
-  int y_bottom = y_top + rect_height + 10;
-  hagl_fill_rectangle(x, y_bottom, x + rect_width - 1, y_bottom + rect_height - 1, red);
-
-  lcd_copy();
 
   /* USER CODE END 2 */
 
